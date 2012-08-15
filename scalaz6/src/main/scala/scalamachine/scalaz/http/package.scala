52c72a796d5c914f2e1d7a3795a60e88eae42e58
Aaron Schlesinger <arschles@gmail.com>
2012-08-15 15:49:20 -0700

    adding scalaz7 Equal instance for HTTPMethod cc/ @jrwest

--- scalaz6/src/main/scala/scalamachine/scalaz/http/package.scala
+++ scalaz6/src/main/scala/scalamachine/scalaz/http/package.scala
@@ -2,16 +2,6 @@ package scalamachine.scalaz
 
 import scalamachine.core.HTTPMethod
 
-/**
- * Created by IntelliJ IDEA.
- *
- * scalamachine.scalaz.http
- *
- * User: aaron
- * Date: 8/8/12
- * Time: 2:28 PM
- */
-
 package object http {
   import scalaz._
   import Scalaz._
