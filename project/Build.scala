9a2063066834087100ed30bbd447d1239110630b
Taylor Leese <taylor@stackmob.com>
2012-12-12 17:42:04 -0800

    Fix build issues * Move to sbt 0.12.0 (some plugins aren't available for 0.12.1) * Removed scalaz7 scalamachine.scalaz package. It's not used and doesn't build. We'll move to Scalaz7 when it and Scala 2.10 are final.

--- project/Build.scala
+++ project/Build.scala
@@ -5,25 +5,23 @@ import com.jsuereth.sbtsite._
 import com.jsuereth.git._
 import com.jsuereth.ghpages._
 import SitePlugin._
-import SiteKeys._
 import GitPlugin._
 import GhPages._
 import Keys._
 
 object BuildSettings {
-
   val org = "com.stackmob"
-  val vsn = "0.1.1-SNAPSHOT"
+  val vsn = "0.2.0-SNAPSHOT"
   val scalaVsn = "2.9.1"
 
   lazy val publishSetting = publishTo <<= version { v: String =>
-      val nexus = "https://oss.sonatype.org/"
-      if (v.trim.endsWith("SNAPSHOT"))
-        Some("snapshots" at nexus + "content/repositories/snapshots")
-      else
-        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    val nexus = "https://oss.sonatype.org/"
+    if (v.trim.endsWith("SNAPSHOT")) {
+      Some("snapshots" at nexus + "content/repositories/snapshots")
+    } else {
+      Some("releases" at nexus + "service/local/staging/deploy/maven2")
     }
-
+  }
 
   lazy val publishSettings = Seq(
     publishSetting,
@@ -31,89 +29,96 @@ object BuildSettings {
     pomIncludeRepository := { x => false },
     pomExtra := (
       <url>https://github.com/stackmob/scalamachine</url>
-        <licenses>
-          <license>
-            <name>Apache 2</name>
-            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>git@github.com:stackmob/scalamachine.git</url>
-          <connection>scm:git:git@github.com:stackmob/scalamachine.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>jrwest</id>
-            <name>Jordan West</name>
-            <url>http://github.com/jrwest</url>
-          </developer>
-        </developers>
-      )
+      <licenses>
+        <license>
+          <name>Apache 2</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:stackmob/scalamachine.git</url>
+        <connection>scm:git:git@github.com:stackmob/scalamachine.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>jrwest</id>
+          <name>Jordan West</name>
+          <url>http://github.com/jrwest</url>
+        </developer>
+        <developer>
+          <id>taylorleese</id>
+          <name>Taylor Leese</name>
+          <url>http://github.com/taylorleese</url>
+        </developer>
+      </developers>
+    )
   )
 
   val standardSettings = Defaults.defaultSettings ++ Seq(
     organization := org,
     version := vsn,
     scalaVersion := scalaVsn,
-    resolvers += ("twitter repository" at "http://maven.twttr.com"),
     shellPrompt <<= ShellPrompt.prompt,
     scalacOptions ++= Seq("-deprecation", "-unchecked"),
-    testOptions in Test += Tests.Argument("html console"),
+    testOptions in Test += Tests.Argument("html", "console"),
     publishArtifact in Test := false,
+    resolvers += ("twitter repository" at "http://maven.twttr.com"),
     resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   )
 
 }
 
 object Dependencies {
-  lazy val iScalaz        = "com.stackmob"            %% "scalamachine-scalaz-iteratee" % "7.0-SNAPSHOT"    % "compile" withSources()
-  lazy val scalaz7        = "org.scalaz"              %% "scalaz-core"                  % "7.0-SNAPSHOT"    % "compile" withSources()
-  lazy val scalaz6        = "org.scalaz"              %% "scalaz-core"                  % "6.0.3"           % "compile" withSources()
+
+  lazy val iScalaz        = "com.stackmob"            %% "scalamachine-scalaz-iteratee" % "7.0-SNAPSHOT"    % "compile"
+  lazy val scalaz6        = "org.scalaz"              %% "scalaz-core"                  % "6.0.3"           % "compile"
   lazy val slf4j          = "org.slf4j"               % "slf4j-api"                     % "1.6.4"           % "compile"
   // Don't want to keep this dependency long term but for now its fastest way to get date parsing for http
-  lazy val commonsHttp    = "commons-httpclient"      % "commons-httpclient"            % "3.1"             % "compile" withSources()
-  lazy val liftweb        = "net.liftweb"             %% "lift-webkit"                  % "2.4"             % "compile" withSources()
+  lazy val commonsHttp    = "commons-httpclient"      % "commons-httpclient"            % "3.1"             % "compile"
+  lazy val liftweb        = "net.liftweb"             %% "lift-webkit"                  % "2.4"             % "compile"
   lazy val jetty          = "org.eclipse.jetty"       % "jetty-webapp"                  % "7.3.0.v20110203" % "container"
-  lazy val servletApi     = "javax.servlet"           % "servlet-api"                   % "2.5"             % "compile" withSources()
-  lazy val finagle        = "com.twitter"             %% "finagle-http"                 % "1.9.12"          % "compile" withSources()
-  lazy val logback        = "ch.qos.logback"          % "logback-classic"               % "1.0.0"           % "compile" withSources()
-  lazy val specs2         = "org.specs2"              %% "specs2"                       % "1.9"             % "test" withSources()
-  lazy val scalacheck     = "org.scala-tools.testing" %% "scalacheck"                   % "1.9"             % "test" withSources()
-  lazy val mockito        = "org.mockito"             % "mockito-all"                   % "1.9.0"           % "test" withSources()
-  lazy val hamcrest       = "org.hamcrest"            % "hamcrest-all"                  % "1.1"             % "test" withSources()
+  lazy val servletApi     = "javax.servlet"           % "servlet-api"                   % "2.5"             % "compile"
+  lazy val finagle        = "com.twitter"             %% "finagle-http"                 % "1.9.12"          % "compile"
+  lazy val logback        = "ch.qos.logback"          % "logback-classic"               % "1.0.0"           % "compile"
+
+  lazy val specs2         = "org.specs2"              %% "specs2"                       % "1.9"             % "test"
+  lazy val scalacheck     = "org.scala-tools.testing" %% "scalacheck"                   % "1.9"             % "test"
+  lazy val mockito        = "org.mockito"             % "mockito-all"                   % "1.9.0"           % "test"
+  lazy val hamcrest       = "org.hamcrest"            % "hamcrest-all"                  % "1.1"             % "test"
   lazy val pegdown        = "org.pegdown"             % "pegdown"                       % "1.0.2"           % "test"
+
 }
 
 object ScalamachineBuild extends Build {
   import BuildSettings._
   import Dependencies._
 
-  private def updatedRepo(repo: SettingKey[File], remote: SettingKey[String], branch: SettingKey[Option[String]]) =
-       (repo, remote, branch, GitKeys.gitRunner, streams) map { (local, uri, branch, git, s) => 
-         git.updated(remote = uri, cwd = local, branch = branch, log = s.log); 
-         local 
+  private def updatedRepo(repo: SettingKey[File], remote: SettingKey[String], branch: SettingKey[Option[String]]) = {
+    (repo, remote, branch, GitKeys.gitRunner, streams) map { (local, uri, branch, git, s) =>
+      git.updated(remote = uri, cwd = local, branch = branch, log = s.log)
+      local
     }
+  }
 
   val docsRepo = SettingKey[String]("docs-repo", "the remote repo that contains documentation for this project")
 
   lazy val scalamachine = Project("scalamachine", file("."),
     settings = standardSettings ++ publishSettings ++ Seq(publishArtifact in Compile := false),
-    aggregate = Seq(core,scalaz6utils,scalaz7utils,servlet,lift,netty)
+    aggregate = Seq(core, scalaz6utils, servlet, lift, netty)
   )
 
   lazy val core = Project("scalamachine-core", file("core"),
     settings = standardSettings ++ publishSettings ++ site.settings ++ site.jekyllSupport("jekyll") ++ site.includeScaladoc() ++ ghpages.settings ++
       Seq(
         name := "scalamachine-core",
-        libraryDependencies ++= Seq(iScalaz,slf4j,commonsHttp,specs2,scalacheck,mockito,hamcrest,pegdown),
-	git.remoteRepo := "git@github.com:stackmob/scalamachine",
+        libraryDependencies ++= Seq(iScalaz, slf4j, commonsHttp, specs2, scalacheck, mockito, hamcrest, pegdown),
+        git.remoteRepo := "git@github.com:stackmob/scalamachine",
         docsRepo := "git@github.com:stackmob/scalamachine.site",
         git.branch in ghpages.updatedRepository := Some("master"),
         ghpages.updatedRepository <<= updatedRepo(ghpages.repository, docsRepo, git.branch in ghpages.updatedRepository)  
       )
   )
-
 
   lazy val scalaz6utils = Project("scalamachine-scalaz6", file("scalaz6"),
     dependencies = Seq(core),
@@ -121,15 +126,6 @@ object ScalamachineBuild extends Build {
       Seq(
         name := "scalamachine-scalaz6",
         libraryDependencies ++= Seq(scalaz6)
-      )
-  )
-
-  lazy val scalaz7utils = Project("scalamachine-scalaz7", file("scalaz7"),
-    dependencies = Seq(core),
-    settings = standardSettings ++ publishSettings ++
-      Seq(
-        name := "scalamachine-scalaz7",
-        libraryDependencies ++= Seq(scalaz7)
       )
   )
 
@@ -165,7 +161,7 @@ object ScalamachineBuild extends Build {
     settings = standardSettings ++ webSettings ++
       Seq(
         name := "scalamachine-servlet-example",
-        libraryDependencies ++= Seq(jetty,logback)
+        libraryDependencies ++= Seq(jetty, logback)
       )
   )
 
@@ -174,7 +170,7 @@ object ScalamachineBuild extends Build {
     settings = standardSettings ++ webSettings ++
       Seq(
         name := "scalamachine-lift-example",
-        libraryDependencies ++= Seq(jetty,logback)
+        libraryDependencies ++= Seq(jetty, logback)
       )
   )
 
@@ -201,16 +197,15 @@ object ScalamachineBuild extends Build {
 object ShellPrompt {
   val prompt = name(name => { state: State =>
     object devnull extends ProcessLogger {
-      def info(s: => String) {}
-      def error(s: => String) { }
-      def buffer[T](f: => T): T = f
+      override def info(s: => String) { }
+      override def error(s: => String) { }
+      override def buffer[T](f: => T): T = f
     }
     val current = """\*\s+(\w+)""".r
     def gitBranches = ("git branch --no-color" lines_! devnull mkString)
     "%s | %s> " format (
       name,
       current findFirstMatchIn gitBranches map (_.group(1)) getOrElse "-"
-      )
+    )
   })
 }
-
--- scalaz7/src/main/scala/scalamachine/scalaz/http/package.scala
+++
@@ -1,8 +0,0 @@
-package scalamachine.scalaz
-
-import scalamachine.core.HTTPMethod
-import scalaz.Equal
-
-package object http {
-  implicit val httpMethodEqual: Equal[HTTPMethod] = Equal.equalA[HTTPMethod]
-}
