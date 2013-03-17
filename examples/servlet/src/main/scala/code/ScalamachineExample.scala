3ff0bebb74d4018d0f73c89db23c3e86f0cb21b9
Douglas Rapp <drapp@stackmob.com>
2013-03-17 00:47:19 -0700

    fix a little compile error

--- examples/servlet/src/main/scala/code/ScalamachineExample.scala
+++ examples/servlet/src/main/scala/code/ScalamachineExample.scala
@@ -2,13 +2,13 @@ package code
 
 import scalamachine.servlet.ServletWebmachineV3
 import scalamachine.core.dispatch.Route._
-import scalamachine.core.{ValueRes, ReqRespData, Resource}
+import scalamachine.core.{Request, ValueRes, ReqRespData, Resource}
 import scalamachine.core.dispatch.DispatchLogging
 
 class EmptyResource extends Resource
 
 class UnavailableResource extends Resource {
-  override def serviceAvailable(data: ReqRespData) = (data, ValueRes(false))
+  override def serviceAvailable(req: Request) = ValueRes(false)
 }
 
 class ScalamachineExample extends ServletWebmachineV3 with DispatchLogging {
